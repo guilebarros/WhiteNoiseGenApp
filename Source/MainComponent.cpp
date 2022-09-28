647d338837027487f5b5c7da9199498dc85fe955
guilebarros <barros.guilherme@gmail.com>
2022-09-28 10:33:44 -0300

    Implemented Slider and Label with gain control

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -2,8 +2,14 @@
 
 MainComponent::MainComponent()
 {
+    levelSlider.setRange(0.0f, 0.25);
+    levelSlider.setTextBoxStyle(juce::Slider::TextBoxRight, false, 100, 20);
+    levelLabel.setText("Noise Level", juce::dontSendNotification);
 
-    setSize (800, 600);
+    addAndMakeVisible(levelSlider);
+    addAndMakeVisible(levelLabel);
+
+    setSize(600, 100);
     
 
  
@@ -29,6 +35,11 @@ MainComponent::~MainComponent()
 
 void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
 {
+    juce::String message;
+    message << "Preparando para tocar audio...\n";
+    message << " samples por bloco esperados =  " << samplesPerBlockExpected << "\n";
+    message << " sample rate =  " << sampleRate << "\n";
+    juce::Logger::getCurrentLogger()->writeToLog(message);
     
 }
 
@@ -37,23 +48,23 @@ void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& buffe
 
     bufferToFill.clearActiveBufferRegion();
 
+    auto level = (float)levelSlider.getValue();
+    
     for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); channel++)
     {
         auto* buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
 
         for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
-        {
-            buffer[sample] = random.nextFloat() * 0.25f - 0.125f;
-            DBG(sample);
-        }
+            buffer[sample] = random.nextFloat() * level;
+            
     }
-
-
 
 }
 
 void MainComponent::releaseResources()
 {
+
+    juce::Logger::getCurrentLogger()->writeToLog("encerrando recursos de audio");
 
 }
 
@@ -61,10 +72,12 @@ void MainComponent::releaseResources()
 void MainComponent::paint (juce::Graphics& g)
 {
 
-    g.fillAll(juce::Colours::transparentBlack);
+    g.fillAll(juce::Colours::black);
 }
 
 void MainComponent::resized()
 {
+    levelLabel.setBounds(10, 10, 90, 20);
+    levelSlider.setBounds(100, 10, getWidth() - 110, 20);
 
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -22,6 +22,8 @@ public:
 private:
 
     juce::Random random;
+    juce::Slider levelSlider;
+    juce::Label levelLabel;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
